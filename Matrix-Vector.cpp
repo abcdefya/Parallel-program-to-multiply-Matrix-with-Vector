1e28aa5df4f025062edb4ab1a198e08e8d2117f1
abcdefya <anh.dothe47@gmail.com>
2023-12-04 10:53:56 +0700

    Added more comments

--- Matrix-Vector.cpp
+++ Matrix-Vector.cpp
@@ -6,211 +6,243 @@
 #include <omp.h>
 #include <math.h>
 #include <fstream>
-#define THREADS 20
 
 using namespace std;
 
-//default sample size is 1, unless the user specifies a value
+/*-----------------------GLOBAL VARIABLES DECLARATION --------------------------*/
+int THREADS = 32;
 int SAMPLES = 1;
+int matrixSize = 1;
+// Dynamic memory allocation for matrices
+double** matrixA;
+double* vectorB;
+double* vectorC;
 
-//MatrixA and VectorB will  be the input Matrices - maximum size 2000
-double matrixA[10000][10000];
-double vectorB[10000];
-double transposeB[10000];
+/* ------------------------- FUNCTION PROTOTYPES ----------------------------- */
+void generate_matrix(int n); // Function to generate a matrix with random values
+double serial_multiplication(int n); // This function performs serial matrix-vector multiplication.
+double parallel_for_multiplication(int n); // This function performs serial matrix-vector multiplication
+void control_menu(); // Function to control the main menu for program selection
+void serial_Selection(); // Function to perform operations in the serial selection mode
+void parallel_Selection(); // Function to perform operations in the parallel selection mode
 
-//this matrix will be the multiplication of Matrix A and B
-double vectorC[10000];
 
-//method header initialization
-void transpose(int n);
-void populate_matrix(int n);
-double serial_multiplication(int n);
-double parallel_for_multiplication(int n);
-double parallel_for_multiplication_optimized(int n);
-
-//these methods will run the tasks according to the given excersice "Lab 3 and 4.pdf"
-void step4_1();
-void step4_2();
+/* ------------------------- CODE FOR EACH FUNCTION----------------------------- */
 
 /*
-This is the main method of the program
+--------This function generates a random matrix (matrixA) and vectors (vectorB, transposeB, vectorC).--------
+It uses a different seed for each experiment to ensure different random values.
+Parameters:
+- n: Size of the square matrix and vectors.
+*/
+void generate_matrix(int n) {
+    // Use a different seed for each experiment to get different random values
+    srand(time(NULL));
+
+    // Allocate memory for the matrix and vectors
+    matrixA = new double*[n];
+    for (int i = 0; i < n; i++) {
+        matrixA[i] = new double[n];
+    }
+
+    vectorB = new double[n];
+    vectorC = new double[n];
+
+    // Generate random values for the matrix and vectors
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            // Assign random values in the range [-500, 500] to matrix elements
+            matrixA[i][j] = rand() % 1000 - 500;
+        }
+        // Assign random values in the range [-500, 500] to vectorB elements
+        vectorB[i] = rand() % 1000 - 500;
+    }
+}
+
+
+/*
+-------- This function performs serial matrix-vector multiplication. ---------
+Parameters:
+- n: Size of the square matrix and vectors.
+Returns:
+- The execution time of the matrix-vector multiplication in seconds.
+*/
+double serial_multiplication(int n) {
+    // Required variable initialization
+    int i, j;
+
+    // Start time measurement using the wall clock
+    double startTime = omp_get_wtime();
+
+    // Perform serial matrix-vector multiplication
+    for (i = 0; i < n; i++) {
+        // Initialize the result vector element to zero
+        vectorC[i] = 0;
+
+        // Perform the dot product of row i of matrixA and vectorB
+        for (j = 0; j < n; j++) {
+            vectorC[i] += matrixA[i][j] * vectorB[j];
+        }
+    }
+
+    // Get the end time from the wall clock
+    double endTime = omp_get_wtime();
+
+    // Return the execution time of the matrix-vector multiplication
+    return endTime - startTime;
+}
+
+
+/*
+--------- This function performs parallel for loop matrix-vector multiplication using OpenMP. -----------
+Parameters:
+- n: Size of the square matrix and vectors.
+Returns:
+- The execution time of the parallel matrix-vector multiplication in seconds.
+*/
+double parallel_for_multiplication(int n) {
+    // Start time measurement using the wall clock
+    double startTime = omp_get_wtime();
+
+        // Set the number of threads
+    omp_set_num_threads(THREADS);
+
+    // Use OpenMP to parallelize the for loop
+    #pragma omp parallel for shared(matrixA, vectorB, vectorC) schedule(static) num_threads(THREADS)
+    for (int i = 0; i < n; i++) {
+        // Initialize the result vector element to zero
+        vectorC[i] = 0;
+
+        // Perform the dot product of row i of matrixA and vectorB
+        for (int j = 0; j < n; j++) {
+            vectorC[i] += matrixA[i][j] * vectorB[j];
+        }
+    }
+
+    // Get the end time from the wall clock
+    double endTime = omp_get_wtime();
+
+    // Return the execution time of the parallel matrix-vector multiplication
+    return endTime - startTime;
+}
+
+
+/*
+This function runs the serial program for matrix-vector multiplication with user input.
+It prompts the user for the matrix size, performs the serial multiplication for a specified number of samples,
+calculates the average time, and prints the result.
+*/
+void serial_Selection() {
+    double totalElapsedTime = 0;
+
+    // Perform matrix-vector multiplication for a specified number of samples
+    for (int i = 0; i < SAMPLES; i++) {
+        // Generate a random matrix and vectors
+        generate_matrix(matrixSize);
+        
+        // Measure the time taken for serial matrix-vector multiplication and accumulate
+        totalElapsedTime += serial_multiplication(matrixSize);
+
+        // Free allocated memory outside the loop to prevent unnecessary deallocations
+        for (int i = 0; i < matrixSize; i++) {
+            delete[] matrixA[i];
+        }
+        delete[] matrixA;
+        delete[] vectorB;
+        delete[] vectorC;
+    }
+
+    // Calculate the average time taken for matrix-vector multiplication
+    double average = totalElapsedTime / SAMPLES;
+
+    // Print the result
+    printf("Serial: Time taken for a %d x %d Matrix-Vector multiplication is %f\n", matrixSize, matrixSize, average);
+    printf("\n---------------------------------------------\n");
+}
+
+
+/*
+-------- This function runs the parallel program for matrix-vector multiplication with user input. ----------
+It prompts the user for the matrix size, performs parallel multiplication for a specified number of samples,
+calculates the average time, and prints the result.
+*/
+void parallel_Selection() {
+    double totalElapsedTime = 0;
+
+    // Perform parallel matrix-vector multiplication for a specified number of samples
+    for (int i = 0; i < SAMPLES; i++) {
+        // Generate a random matrix and vectors
+        generate_matrix(matrixSize);
+
+        // Measure the time taken for parallel matrix-vector multiplication and accumulate
+        totalElapsedTime += parallel_for_multiplication(matrixSize);
+
+        // Free allocated memory outside the loop to prevent unnecessary deallocations
+        for (int i = 0; i < matrixSize; i++) {
+            delete[] matrixA[i];
+        }
+        delete[] matrixA;
+        delete[] vectorB;
+        delete[] vectorC;
+    }
+
+    // Calculate the average time taken for parallel matrix-vector multiplication
+    double average = totalElapsedTime / SAMPLES;
+
+    // Print the result
+    printf("Parallel: Time taken for a %d x %d Matrix-Vector multiplication is %f\n", matrixSize, matrixSize, average);
+    printf("\n---------------------------------------------\n");
+}
+
+
+/*-------- Function to control the main menu for program selection ---------*/ 
+void control_menu() {
+    int selection;
+
+    // Infinite loop to continuously prompt the user for program selection
+    while (true) {
+        printf("Enter one of the options given below\n \t1 - For serial program \n\t2 - For parallel program\n \t3 - Exit\n");
+        scanf("%d", &selection);
+
+        // Exit the loop and program if the user selects option 3
+        if (selection == 3) return;
+
+        // Prompt the user to enter the sample size
+        printf("Enter sample size: ");
+        scanf("%d", &SAMPLES);
+
+        // Matrix Size input
+        printf("Enter matrixSize: ");
+        if (scanf("%d", &matrixSize) != 1) {
+            printf("Invalid input for matrixSize. Exiting...\n");
+            return;
+        }
+
+         // Prompt the user to input the number of threads
+        printf("Enter the number of threads (default is %d): ", THREADS);
+        scanf("%d", &THREADS);
+
+        // Perform actions based on user's selection
+        switch (selection) {
+            case 1:
+                // Call the function to perform operations in serial mode
+                serial_Selection();
+                break;
+
+            case 2:
+                // Call the function to perform operations in parallel mode
+                parallel_Selection();
+                break;
+        }
+    }
+} // End function
+
+/*
+----------------------------- MAIN FUNCTION -------------------------
 */
 int main() {
-
-	char method;
-
-	//Enter method that needs to be run
-	printf("Enter one of the options given below\n \ts - for serial program \n\tp - for parallel program \n\to - for optimized program\n");
-	scanf("%c", &method);
-
-	//Enter sample size for the above selected method
-	printf("Enter sample size: ");
-	scanf("%d", &SAMPLES);
-
-	switch (method) {
-		case 's':
-			step4_1();
-			break;
-
-		case 'p':
-			step4_2();
-			break;
-		
-				
-	}
-	
-	return 0;
-
-
-}
-
-/*
-This method will get the transpose of vectorB
-*/
-void transpose(int n) {
-    int i;
-    for(i=0; i<n; i++) {
-        transposeB[i] = vectorB[i];
-    }
-}
-
-
-/*
-This method will populate the matrix with random numbers
-*/
-void populate_matrix(int n){
-
-	//different seed for each experiment
-	srand(time(NULL));
-
-	//populate matrices
-	for (int i = 0; i < n; i++){
-		for (int j = 0; j < n; j++){
-			matrixA[i][j] = rand() % 50;
-		}
-		vectorB[i] = rand() % 50;
-	}
-
-}
-
-/*
-This methid will do serial multiplication
-*/
-double serial_multiplication(int n){
-
-	//required variable initilization
-	int i, j;
-
-	//start time from the wall clock
-	double startTime = omp_get_wtime();
-	
-	for (i = 0; i < n; i++){
-		vectorC[i] = 0;
-		for (j = 0; j < n; j++){
-			vectorC[i] +=  matrixA[i][j] * vectorB[j];
-		}
-	}
-
-	//get the end time from the wall clock
-	double endTime = omp_get_wtime();
-
-	//return the execution time
-	return endTime - startTime;
-}
-
-/*
-This method will do parallel for loop multiplication
-*/
-double parallel_for_multiplication(int n){
-		
-	//start time from the wall clock
-	double startTime = omp_get_wtime();
-
-	//for loop spefication with shared, private variables and number of threads
-	#pragma omp parallel for shared(matrixA,  vectorB, vectorC) schedule(static) num_threads(THREADS)
-	for (int i = 0; i < n ; i++ ){
-		vectorC[i] = 0;
-		for (int j = 0; j < n ; j++ ){
-			vectorC[i] = vectorC[i] + matrixA[i][j]*vectorB[j];
-		}
-	}
-
-	//get the end time from wall clock
-	double endTime = omp_get_wtime();
-
-	//return the execution time
-	return endTime - startTime;
-
-}
-
-void writeMatrixToFile(const char* filename, double matrix[][10000], int rows, int cols) {
-    ofstream outFile(filename);
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            outFile << matrix[i][j] << " ";
-        }
-        outFile << endl;
-    }
-
-    outFile.close();
-}
-
-void writeVectorToFile(const char* filename, double vector[], int size) {
-    ofstream outFile(filename);
-
-    for (int i = 0; i < size; i++) {
-        outFile << vector[i] << endl;
-    }
-
-    outFile.close();
-}
-
-
-void step4_1() {
-    int n;
-    printf("Enter matrix size: ");
-    scanf("%d", &n);
-
-    populate_matrix(n);
-    // Calculate execution time for serial multiplication
-	double time = serial_multiplication(n);
-	printf("Serial: Time taken for a %d x %d matrix-vector multiplication is %f s\n", n, n, time);
-    // Print matrix A to A.txt
-    writeMatrixToFile("A.txt", matrixA, n, n);
-
-    // Print vector B to B.txt
-    writeVectorToFile("B.txt", vectorB, n);
-
     
-    
-    // Print result vector C to C.txt
-    writeVectorToFile("C.txt", vectorC, n);
-
-    
+    control_menu();
+    return 0;
 }
-
-void step4_2() {
-    int n;
-    printf("Enter matrix size: ");
-    scanf("%d", &n);
-
-    populate_matrix(n);
-	// Calculate execution time for parallel multiplication
-    double time = parallel_for_multiplication(n);
-     printf("Parallel: Time taken for a %d x %d matrix-vector multiplication is %f s\n", n, n, time);
-    // Print matrix A to A.txt
-    writeMatrixToFile("A.txt", matrixA, n, n);
-
-    // Print vector B to B.txt
-    writeVectorToFile("B.txt", vectorB, n);
-
-    
-
-    // Print result vector C to C.txt
-    writeVectorToFile("C.txt", vectorC, n);
-
-   
-}
-
 
